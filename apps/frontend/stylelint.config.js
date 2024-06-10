1ea03005bc9a71747e4dacad832eb2d2a6beafc5
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2024-06-10 08:47:03 +0300

    thjs-122: * stylelint configuration for workspaces

--- apps/frontend/stylelint.config.js
+++ apps/frontend/stylelint.config.js
@@ -3,11 +3,16 @@ const config = {
   extends: ['../../stylelint.config.js', 'stylelint-config-standard-scss'],
   plugins: ['stylelint-scss'],
   rules: {
+    'color-hex-length': 'long',
+    'declaration-no-important': true,
+    'max-nesting-depth': 0,
+    'no-descending-specificity': true,
     'selector-class-pattern': null,
     'selector-pseudo-class-no-unknown': [
       true,
       { ignorePseudoClasses: ['global'] }
-    ]
+    ],
+    'unit-disallowed-list': ['em', 'rem']
   }
 };
 
